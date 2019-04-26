8f589f36ecef818cb17e0d8540949186281adf77
Phil Tietjen <ptietjen@labx.com>
2019-04-26 17:04:26 -0400

    add content for about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -32,15 +32,38 @@ const About = () => (
     <Interests>
       <InterestCard>
         <GiGamepad />
+        <CardText>
+          I've been playing video games since I was a very young age, and is
+          still a passtime that has stuck with me in adulthood, granted with
+          much less time to spend with them. When i do find time, I'm generally
+          a sucker for RPG's,action games or co-op games with my brothers.
+        </CardText>
       </InterestCard>
       <InterestCard>
         <GiBeerStein />
+        <CardText>
+          As long as I've been drinking (Age will remain unspecified), I've
+          always enjoyed a beer, however in more recent years I've grown into
+          the craft beer scene. So far I've found that I like a good sour or a
+          stout. I like other stuff like IPA's too, just generally not AS much.
+        </CardText>
       </InterestCard>
       <InterestCard>
         <GiCoffeeMug />
+        <CardText>
+          Not much to say here. I like to drink coffee, I like the taste and it
+          keeps me awake when I need to be.
+        </CardText>
       </InterestCard>
       <InterestCard contentCenter={true}>
         <GiWeightLiftingDown />
+        <CardText>
+          Fitness is important to me, and weight lifting in general is fun to
+          me. It's very rewarding and getting better at moving heavy objects
+          tickles something in my brain i guess. Small children have made it
+          harder to keep up, but it's something I always try to squeeze in to me
+          schedule, especially while I have a desk job.
+        </CardText>
       </InterestCard>
     </Interests>
   </Layout>
@@ -51,14 +74,16 @@ const Interests = styled.div`
 `
 
 const InterestCard = styled(Card)`
-  font-size: 5rem;
+  svg {
+    font-size: 5rem;
+  }
   margin: 1rem 0;
   flex-basis: 100%;
   @media screen and (min-width: 576px) {
     flex-basis: 48%;
   }
   @media screen and (min-width: 920px) {
-    flex-basis: 23%;
+    flex-basis: 48%;
   }
   svg {
     margin: auto;
