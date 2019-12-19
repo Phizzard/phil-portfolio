526e66b6a69e1ba834feba226514f40796168dee
Phil Tietjen <tietjen.philip@gmail.com>
2019-12-18 23:10:42 -0500

    fix headshot image size

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -19,7 +19,7 @@ const Image = () => (
       query {
         placeholderImage: file(relativePath: { eq: "head-shot.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 300) {
+            fluid(maxWidth: 768) {
               ...GatsbyImageSharpFluid
             }
           }
