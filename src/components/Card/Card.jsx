5d7c6b00296e6cfb7a44fb34757c49582c661ceb
Phil Tietjen <tietjen.philip@gmail.com>
2019-12-18 23:11:09 -0500

    add fluid image support to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,10 @@
 import React from "react"
-import { any } from "prop-types"
+import { any, string, shape, boolean, object } from "prop-types"
 import styled from "@emotion/styled"
 import tw from "../../../tailwind"
+import GatsbyImage from "gatsby-image"
 
-const Card = ({
-  title,
-  children,
-  className,
-  image,
-  imageTop,
-  contentCenter,
-}) => (
+const Card = ({ title, children, className, image, contentCenter }) => (
   <Container className={className}>
     {title && (
       <Header>
@@ -19,8 +13,12 @@ const Card = ({
     )}
     <Body>
       {image && (
-        <ImageWrapper imageTop={imageTop}>
-          <Image src={image} />
+        <ImageWrapper imageTop={image.top}>
+          {image.fluid ? (
+            <StyledGatsbyImage fluid={image.fluid} />
+          ) : (
+            <Image src={image.src} />
+          )}
         </ImageWrapper>
       )}
       <Content contentCenter={contentCenter}>{children}</Content>
@@ -61,15 +59,32 @@ const ImageWrapper = styled.div`
 const Image = styled.img`
   ${tw`max-w-xs max-h-xs m-0 w-full`}
 `
+
+const StyledGatsbyImage = styled(GatsbyImage)`
+  ${tw`max-w-xs max-h-xs m-0 w-full mx-auto`}
+`
 const Content = styled.div`
   ${tw`p-2 w-full flex-1 ${props =>
     props.contentCenter ? "text-center" : "text-left"}`}
 `
 Card.defaultProps = {
-  imageTop: false,
+  image: {
+    top: false,
+    src: "",
+    fluid: {},
+  },
 }
+
 Card.propTypes = {
   children: any,
+  title: string,
+  className: string,
+  image: shape({
+    top: boolean,
+    src: string,
+    fluid: object,
+  }),
+  contentCenter: boolean,
 }
 
 export default Card
